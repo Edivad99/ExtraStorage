a71af5d883ed08bc2abe93a26be92ca199d3ff55
Davide Albiero <davide2910@live.it>
2022-07-20 17:49:50 +0200

    Revert "Fix eventbus"
    This reverts commit 68de20f6e02f3f9c134c4d38477982180b463788.

--- src/main/java/edivad/extrastorage/Main.java
+++ src/main/java/edivad/extrastorage/Main.java
@@ -5,7 +5,9 @@ import edivad.extrastorage.setup.ClientSetup;
 import edivad.extrastorage.setup.Config;
 import edivad.extrastorage.setup.ModSetup;
 import edivad.extrastorage.setup.Registration;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
@@ -22,12 +24,12 @@ public class Main
 
     public Main()
     {
+        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> ClientSetup::new);
         Registration.init();
         Config.init();
 
         // Register the setup method for modloading
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
         eventBus.addListener(ModSetup::init);
-        eventBus.addListener(ClientSetup::init);
     }
 }

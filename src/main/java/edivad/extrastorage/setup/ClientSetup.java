07836206ac4e1e9ea77ca5934e56ead143019a4b
Davide Albiero <davide2910@live.it>
2025-07-05 09:20:37 +0200

    Fix #95

--- src/main/java/edivad/extrastorage/setup/ClientSetup.java
+++ src/main/java/edivad/extrastorage/setup/ClientSetup.java
@@ -3,6 +3,7 @@ package edivad.extrastorage.setup;
 import com.refinedmods.refinedstorage.common.api.RefinedStorageClientApi;
 import com.refinedmods.refinedstorage.common.support.resource.FluidResource;
 import com.refinedmods.refinedstorage.common.support.resource.ItemResource;
+import com.refinedmods.refinedstorage.common.util.IdentifierUtil;
 import edivad.edivadlib.setup.UpdateChecker;
 import edivad.extrastorage.ExtraStorage;
 import edivad.extrastorage.advancedexporter.AdvancedExporterScreen;
@@ -24,6 +25,16 @@ public class ClientSetup {
 
   public static void handleClientSetup(FMLClientSetupEvent event) {
     NeoForge.EVENT_BUS.register(new UpdateChecker(ExtraStorage.ID));
+
+    for (var value : ESItems.ITEM_DISK.values()) {
+      RefinedStorageClientApi.INSTANCE.registerDiskModel(
+          value.asItem(), IdentifierUtil.createIdentifier("block/disk/disk"));
+    }
+
+    for (var value : ESItems.FLUID_DISK.values()) {
+      RefinedStorageClientApi.INSTANCE.registerDiskModel(
+          value.asItem(), IdentifierUtil.createIdentifier("block/disk/fluid_disk"));
+    }
   }
 
   public static void handleRegisterMenuScreens(RegisterMenuScreensEvent event) {
